875a1c49fd23a80c700f0ea8fb7014153b45de9a
codehruv <codehruv@users.noreply.github.com>
2026-09-03 01:16:00 -0400

    share 0.2.1

--- Formula/share.rb
+++ Formula/share.rb
@@ -1,8 +1,8 @@
 class Share < Formula
   desc "Send folders/files to friends using croc"
   homepage "https://github.com/dhruv-hhai/share"
-  url "https://github.com/dhruv-hhai/share/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "e2feba22e3d7943837500618daa6cae47426138bbd0ca3d01648eeab75eb4d60"
+  url "https://github.com/dhruv-hhai/share/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "0409582dd9fe9870260aa09fd5c62aae638566a304013683a6bfe58b83292306"
   license "MIT"
 
   depends_on "croc"
